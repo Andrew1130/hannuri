448181692f8a3ce93d3452360dd4144b1035e792
Andrew1130 <yhyh273@gmail.com>
2022-05-02 19:57:18 +0900

    Hotfix 2
    main_import.js 파일 내에slideBox_modal.js가 호출되는 위치 수정

--- js/src/main_import.js
+++ js/src/main_import.js
@@ -73,10 +73,9 @@ if(deviceCk === 'smartphone'){
       body.append('<script src="../js/prev_ver/main_page/smartphone/slideBox.js" class="slideBox_smartphone smartphone_js"></script>')
       body.append('<script src="../js/prev_ver/main_page/smartphone/booklistBox.js" class="slideBox_smartphone smartphone_js"></script>')
     } else {
-      // 익스플로러가 아닐 경우 호출되는 스크립트
+      // 익스플로러, 파이어폭스가 아닐 경우 호출되는 스크립트
       body.append('<script src="../js/main_page/smartphone/slideBox_cardMake.js" class="slideBox_smartphone smartphone_js"></script>')
       body.append('<script src="../js/main_page/smartphone/slideBox_slide_swipe.js" class="slideBox_smartphone smartphone_js"></script>')
-      body.append('<script src="../js/main_page/smartphone/slideBox_modal.js" class="slideBox_smartphone smartphone_js"></script>')
       body.append('<script src="../js/main_page/smartphone/booklistBox_cardMake.js" class="booklistBox_smartphone smartphone_js"></script>')
       body.append('<script src="../js/main_page/smartphone/booklistBox_slide.js" class="booklistBox_smartphone smartphone_js"></script>')
     }
@@ -86,6 +85,7 @@ if(deviceCk === 'smartphone'){
   body.append('<script src="../js/main_page/smartphone/introBox_slide.js" class="introBox_smartphone smartphone_js"></script>')
   body.append('<script src="../js/main_page/smartphone/eventBox_cardMake.js" class="eventBox_smartphone smartphone_js"></script>')
   body.append('<script src="../js/main_page/smartphone/eventBox_accordion.js" class="eventBox_smartphone smartphone_js"></script>')
+  body.append('<script src="../js/main_page/smartphone/slideBox_modal.js" class="slideBox_smartphone smartphone_js"></script>')
 
 
 } else if (deviceCk === 'tablet') {
@@ -100,7 +100,7 @@ if(deviceCk === 'smartphone'){
     body.append('<script src="../js/prev_ver/main_page/tablet/booklistBox.js" class="booklistBox_tablet tablet_js"></script>')
   }
   else {
-    // 익스플로러가 아닐 경우 호출되는 스크립트
+    // 익스플로러, 파이어폭스가 아닐 경우 호출되는 스크립트
     body.append('<script src="../js/main_page/tablet/slideBox_cardMake.js" class="slideBox_tablet tablet_js"></script>')
     body.append('<script src="../js/main_page/tablet/slideBox_slide.js" class="slideBox_tablet tablet_js"></script>')
     body.append('<script src="../js/main_page/tablet/booklistBox_cardMake.js" class="booklistBox_tablet tablet_js"></script>')
